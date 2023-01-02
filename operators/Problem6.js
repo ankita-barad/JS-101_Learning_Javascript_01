9fec85033233d38ce04492eb6774d79fe154195f
ankita-barad <ankitabarad2111@gmail.com>
2023-01-02 23:18:58 +0530

    operators_day2

---
+++ operators/Problem6.js
@@ -0,0 +1,15 @@
+let x=10;
+let y=20;
+let z="30";
+let v="40";
+
+console.log(x+y,typeof(x+y));
+console.log(y+z,typeof(y+z));
+console.log(z+v,typeof(z+v));
+console.log(10%20)
+console.log((10+2)**2)
+
+//concatination
+//number+number=number
+//number+string=string
+//string+string=string
---
+++ operators/Problem7.js
@@ -0,0 +1,2 @@
+let married = false;
+console.log(married)
